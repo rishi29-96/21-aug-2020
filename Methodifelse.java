035b1b2c3c3657db875fb5fdd2e5aa2bc899e9fe
rishi29-96 <69304648+rishi29-96@users.noreply.github.com>
2020-08-21 18:32:10 +0530

    Add files via upload

---
+++ Methodifelse.java
@@ -0,0 +1,17 @@
+public class Methodifelse {
+
+  static void checkAge(int age) {
+    if (age < 18) {
+      System.out.println("Access denied - You are not old enough!"); 
+      
+    
+    } else {
+      System.out.println("Access granted - You are old enough!"); 
+    }
+    
+  } 
+
+  public static void main(String[] args) { 
+    checkAge(20); 
+  } 
+}
